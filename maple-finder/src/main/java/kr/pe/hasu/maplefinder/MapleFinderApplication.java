d2f2c260d5bae351972105be7df9bab432496e05
hasu <jun2077681@naver.com>
2021-05-25 16:08:53 +0900

    init project

---
+++ maple-finder/src/main/java/kr/pe/hasu/maplefinder/MapleFinderApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.hasu.maplefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleFinderApplication.class, args);
+	}
+
+}
---
+++ maple-finder/src/test/java/kr/pe/hasu/maplefinder/MapleFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.hasu.maplefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
